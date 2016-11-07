240d832a78b9e97de89c2e7a0c3a127c85f79dc4
Michael Astrauckas <mastrauckas@hotmail.com>
2016-11-07 06:41:53 -0500

    Adding test for the endpoint.

--- src/wwwroot/app.py
+++ src/wwwroot/app.py
@@ -1,23 +1,41 @@
-from flask import Flask, jsonify, json, render_template, Response
+from flask import Flask, jsonify, json, render_template, Response, request
 from mortgage import Mortgage
 
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/api/ping")
 def index():
     return render_template('main.html')
 
 
 @app.route("/api/v1.0/mortgage", methods=['GET'])
 def get_mortgage():
-    mortgage = Mortgage(10, 398483, 3.12)
-    installments = mortgage.get_All_Installments(40420.35)
-    r = json.dumps({ "Installments": [toJson(i) for i in installments] })
-    return Response(r, content_type='text/json; charset=utf-8')
+    missing_Parameters = check_all_parameters_exist()
 
-def toJson(installment):
+    if len(missing_Parameters) > 0:
+        r = json.dumps({ "validation_errors": [parameter_Missing_To_Json(p) for p in missing_Parameters]  })
+        return Response(r, status=422, content_type='application/json')
+
+    parameters_Not_Valid = {}
+    parameters_Valid = {}
+    check_Not_Valid_Parameters(parameters_Valid, parameters_Not_Valid)
+    if(len(parameters_Not_Valid) > 0):
+        r = json.dumps({ "validation_errors": [parameter_Not_Valid_To_Json(key, value) for key, value in parameters_Not_Valid.items()]  })
+        return Response(r, status=422, content_type='application/json')
+
+    total_Installments = parameters_Valid['installments']
+    principal = parameters_Valid['principal']
+    rate = parameters_Valid['rate']
+    payment = parameters_Valid['payment']
+
+    mortgage = Mortgage(total_Installments, principal, rate)
+    installments = mortgage.get_All_Installments(payment)
+    r = json.dumps({ "Installments": [installment_To_Json(i) for i in installments] })
+    return Response(r, content_type='application/json')
+
+def installment_To_Json(installment):
     return {
             "number": installment.installment_Number(),
             "principal_amount": installment.principal_Amount(),
@@ -25,6 +43,56 @@ def toJson(installment):
             "total_payment": installment.total_Payment(),
             "amortization_error_amount": installment.amortization_Error_Amount()
             }
+
+def parameter_Missing_To_Json(missing_Parameter_Name):
+    return {
+            "parameter": missing_Parameter_Name,
+            "error_code": '001',
+            "error_message": 'Parameter \'' + missing_Parameter_Name + '\' missing from request.'
+            }
+
+def parameter_Not_Valid_To_Json(invalid_Parameter_Name, correct_Type):
+    return {
+            "parameter": invalid_Parameter_Name,
+            "error_code": '002',
+            "error_message": 'Parameter \'' + invalid_Parameter_Name + '\' not the correct type.',
+            "correct_type": correct_Type
+            }
+
+def check_all_parameters_exist():
+    missing_parameters = []
+    if request.args.get('installments') is None:
+        missing_parameters.append('installments')
+    if request.args.get('principal') is None:
+        missing_parameters.append('principal')
+    if request.args.get('rate') is None:
+        missing_parameters.append('rate')
+    if request.args.get('payment') is None:
+        missing_parameters.append('payment')
+
+    return missing_parameters
+
+def check_Not_Valid_Parameters(parameters_Valid, parameters_Not_Valid):
+    try:
+        parameters_Valid["installments"] = int(request.args.get('installments'))
+    except ValueError:
+        parameters_Not_Valid["installments"] = "int"
+
+    try:
+        parameters_Valid["principal"] = float(request.args.get('principal'))
+    except ValueError:
+        parameters_Not_Valid["principal"] = "float"
+
+    try:
+        parameters_Valid["rate"] = float(request.args.get('rate'))
+    except ValueError:
+        parameters_Not_Valid["rate"] = "float"
+
+    try:
+        parameters_Valid["payment"] = float(request.args.get('payment'))
+    except ValueError:
+        parameters_Not_Valid["payment"] = "float"
+
 
 if __name__ == "__main__":
     app.run()
---
+++ tests/py/test_endpoint_missing_parameters.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+from flask import Flask, url_for, Response, json, jsonify
+from app import app as mortgage_web_app
+import pytest
+
+
+@pytest.mark.usefixtures('client_class')
+class TestEndpointMissingParameters:
+
+    @pytest.fixture
+    def app(self):
+        mortgage_web_app.debug = True
+        mortgage_web_app.response_class = Response
+        return mortgage_web_app
+
+    def test_missing_all_parameters(self, client):
+        response = client.get('/api/v1.0/mortgage')
+        assert response.status_code == 422
+        assert response.content_type == 'application/json'
+        assert isinstance(response.json, dict)
+        assert len(response.json['validation_errors']) == 4
+
+        validation_error = response.json['validation_errors'][0]
+        assert validation_error.get('error_code') == '001'
+        parameter = validation_error.get('parameter')
+        assert parameter == 'installments'
+        assert validation_error.get('error_message') == 'Parameter \'' + parameter + '\' missing from request.'
+
+        validation_error = response.json['validation_errors'][1]
+        assert validation_error.get('error_code') == '001'
+        parameter = validation_error.get('parameter')
+        assert parameter == 'principal'
+        assert validation_error.get('error_message') == 'Parameter \'' + parameter + '\' missing from request.'
+
+        validation_error = response.json['validation_errors'][2]
+        assert validation_error.get('error_code') == '001'
+        parameter = validation_error.get('parameter')
+        assert parameter == 'rate'
+        assert validation_error.get('error_message') == 'Parameter \'' + parameter + '\' missing from request.'
+
+        validation_error = response.json['validation_errors'][3]
+        assert validation_error.get('error_code') == '001'
+        parameter = validation_error.get('parameter')
+        assert parameter == 'payment'
+        assert validation_error.get('error_message') == 'Parameter \'' + parameter + '\' missing from request.'
+
+    def test_missing_installments_parameter(self, client):
+        response = client.get('/api/v1.0/mortgage?rate=3.12&principal=398483&payment=4040.35')
+        assert response.status_code == 422
+        assert response.content_type == 'application/json'
+        assert isinstance(response.json, dict)
+        assert len(response.json['validation_errors']) == 1
+
+        validation_error = response.json['validation_errors'][0]
+        assert validation_error.get('error_code') == '001'
+        parameter = validation_error.get('parameter')
+        assert parameter == 'installments'
+        assert validation_error.get('error_message') == 'Parameter \'' + parameter + '\' missing from request.'
+
+    def test_missing_principal_parameter(self, client):
+        response = client.get('/api/v1.0/mortgage?rate=3.12&installments=10&payment=4040.35')
+        assert response.status_code == 422
+        assert response.content_type == 'application/json'
+        assert isinstance(response.json, dict)
+        assert len(response.json['validation_errors']) == 1
+
+        validation_error = response.json['validation_errors'][0]
+        assert validation_error.get('error_code') == '001'
+        parameter = validation_error.get('parameter')
+        assert parameter == 'principal'
+        assert validation_error.get('error_message') == 'Parameter \'' + parameter + '\' missing from request.'
+
+    def test_missing_rate_parameter(self, client):
+        response = client.get('/api/v1.0/mortgage?installments=10&principal=398483&payment=4040.35')
+        assert response.status_code == 422
+        assert response.content_type == 'application/json'
+        assert isinstance(response.json, dict)
+        assert len(response.json['validation_errors']) == 1
+
+        validation_error = response.json['validation_errors'][0]
+        assert validation_error.get('error_code') == '001'
+        parameter = validation_error.get('parameter')
+        assert parameter == 'rate'
+        assert validation_error.get('error_message') == 'Parameter \'' + parameter + '\' missing from request.'
+
+    def test_missing_payment_parameter(self, client):
+        response = client.get('/api/v1.0/mortgage?rate=3.12&installments=10&principal=398483')
+        assert response.status_code == 422
+        assert response.content_type == 'application/json'
+        assert isinstance(response.json, dict)
+        assert len(response.json['validation_errors']) == 1
+
+        validation_error = response.json['validation_errors'][0]
+        assert validation_error.get('error_code') == '001'
+        parameter = validation_error.get('parameter')
+        assert parameter == 'payment'
+        assert validation_error.get('error_message') == 'Parameter \'' + parameter + '\' missing from request.'
