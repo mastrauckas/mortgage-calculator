a46768ae3dc60b9338688aadfac0e9f1e83e643f
Michael Astrauckas <mastrauckas@hotmail.com>
2017-01-15 11:15:48 -0500

    Doing a bit more formatting.

--- tests/py/test_endpoint_missing_parameters.py
+++ tests/py/test_endpoint_missing_parameters.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from flask import Flask, url_for, Response, json, jsonify
+from flask import Flask, Response
 from app import app as mortgage_web_app
 import pytest
 
@@ -15,6 +15,7 @@ class TestEndpointMissingParameters:
 
     def test_missing_all_parameters(self, client):
         response = client.get('/api/v1.0/mortgage')
+
         assert response.status_code == 422
         assert response.content_type == 'application/json'
         assert isinstance(response.json, dict)
@@ -24,28 +25,38 @@ class TestEndpointMissingParameters:
         assert validation_error.get('error_code') == '001'
         parameter = validation_error.get('parameter')
         assert parameter == 'installments'
-        assert validation_error.get('error_message') == 'Parameter \'' + parameter + '\' missing from request.'
+        assert validation_error.get(
+            'error_message') == 'Parameter \'' + \
+            parameter + '\' missing from request.'
 
         validation_error = response.json['validation_errors'][1]
         assert validation_error.get('error_code') == '001'
         parameter = validation_error.get('parameter')
         assert parameter == 'principal'
-        assert validation_error.get('error_message') == 'Parameter \'' + parameter + '\' missing from request.'
+        assert validation_error.get(
+            'error_message') == 'Parameter \'' + \
+            parameter + '\' missing from request.'
 
         validation_error = response.json['validation_errors'][2]
         assert validation_error.get('error_code') == '001'
         parameter = validation_error.get('parameter')
         assert parameter == 'rate'
-        assert validation_error.get('error_message') == 'Parameter \'' + parameter + '\' missing from request.'
+        assert validation_error.get(
+            'error_message') == 'Parameter \'' + \
+            parameter + '\' missing from request.'
 
         validation_error = response.json['validation_errors'][3]
         assert validation_error.get('error_code') == '001'
         parameter = validation_error.get('parameter')
         assert parameter == 'payment'
-        assert validation_error.get('error_message') == 'Parameter \'' + parameter + '\' missing from request.'
+        assert validation_error.get(
+            'error_message') == 'Parameter \'' + \
+            parameter + '\' missing from request.'
 
     def test_missing_installments_parameter(self, client):
-        response = client.get('/api/v1.0/mortgage?rate=3.12&principal=398483&payment=4040.35')
+        response = client.get(
+            '/api/v1.0/mortgage?rate=3.12&principal=398483&payment=4040.35')
+
         assert response.status_code == 422
         assert response.content_type == 'application/json'
         assert isinstance(response.json, dict)
@@ -55,10 +66,14 @@ class TestEndpointMissingParameters:
         assert validation_error.get('error_code') == '001'
         parameter = validation_error.get('parameter')
         assert parameter == 'installments'
-        assert validation_error.get('error_message') == 'Parameter \'' + parameter + '\' missing from request.'
+        assert validation_error.get(
+            'error_message') == 'Parameter \'' + \
+            parameter + '\' missing from request.'
 
     def test_missing_principal_parameter(self, client):
-        response = client.get('/api/v1.0/mortgage?rate=3.12&installments=10&payment=4040.35')
+        response = client.get(
+            '/api/v1.0/mortgage?rate=3.12&installments=10&payment=4040.35')
+
         assert response.status_code == 422
         assert response.content_type == 'application/json'
         assert isinstance(response.json, dict)
@@ -68,10 +83,15 @@ class TestEndpointMissingParameters:
         assert validation_error.get('error_code') == '001'
         parameter = validation_error.get('parameter')
         assert parameter == 'principal'
-        assert validation_error.get('error_message') == 'Parameter \'' + parameter + '\' missing from request.'
+        assert validation_error.get(
+            'error_message') == 'Parameter \'' + \
+            parameter + '\' missing from request.'
 
     def test_missing_rate_parameter(self, client):
-        response = client.get('/api/v1.0/mortgage?installments=10&principal=398483&payment=4040.35')
+        response = client.get(
+            '/api/v1.0/mortgage?installments=10&' +
+            'principal=398483&payment=4040.35')
+
         assert response.status_code == 422
         assert response.content_type == 'application/json'
         assert isinstance(response.json, dict)
@@ -81,10 +101,14 @@ class TestEndpointMissingParameters:
         assert validation_error.get('error_code') == '001'
         parameter = validation_error.get('parameter')
         assert parameter == 'rate'
-        assert validation_error.get('error_message') == 'Parameter \'' + parameter + '\' missing from request.'
+        assert validation_error.get(
+            'error_message') == 'Parameter \'' + \
+            parameter + '\' missing from request.'
 
     def test_missing_payment_parameter(self, client):
-        response = client.get('/api/v1.0/mortgage?rate=3.12&installments=10&principal=398483')
+        response = client.get(
+            '/api/v1.0/mortgage?rate=3.12&installments=10&principal=398483')
+
         assert response.status_code == 422
         assert response.content_type == 'application/json'
         assert isinstance(response.json, dict)
@@ -94,4 +118,6 @@ class TestEndpointMissingParameters:
         assert validation_error.get('error_code') == '001'
         parameter = validation_error.get('parameter')
         assert parameter == 'payment'
-        assert validation_error.get('error_message') == 'Parameter \'' + parameter + '\' missing from request.'
+        assert validation_error.get(
+            'error_message') == 'Parameter \'' + \
+            parameter + '\' missing from request.'
