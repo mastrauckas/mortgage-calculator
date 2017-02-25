5e42c7804745b099775c6aacb68228185d6f5d7a
Michael Astrauckas <mastrauckas@hotmail.com>
2017-02-25 15:56:51 -0500

    Fixed some issues with the numbers and prop names.

--- src/js-src/components/mortgage/amortizationSchedule.js
+++ src/js-src/components/mortgage/amortizationSchedule.js
@@ -9,43 +9,52 @@ export default class AmortizationSchedule extends Component {
       amortizationSchedule: [
         {
           installmentNumber: 1,
-          dueDate: '1/1/2017',
+          installmentDate: '1/1/2017',
+          payment: 200.54,
           interestAmount: 100.00,
           principalAmount: 200.10,
-          totalDue: 300.10
+          balance: 300.10
         },
         {
           installmentNumber: 2,
-          dueDate: '2/1/2017',
+          installmentDate: '2/1/2017',
+          payment: 200.54,
           interestAmount: 200.11,
           principalAmount: 300.10,
-          totalDue: 400.10
+          balance: 400.10
         },
         {
           installmentNumber: 3,
-          dueDate: '3/1/2017',
+          installmentDate: '3/1/2017',
+          payment: 200.54,
           interestAmount: 300.10,
           principalAmount: 400.10,
-          totalDue: 500.10
+          balance: 500.10
         }
       ],
     };
   }
   render() {
-    const AmortizationScheduleItemComponent = this.state.amortizationSchedule.map(item => {
-      return <AmortizationScheduleItem key={item.installmentNumber} {...item} />;
+    const AmortizationScheduleItemComponent = this.state.amortizationSchedule.map(installment => {
+      return <AmortizationScheduleItem key={installment.installmentNumber} {...installment} />;
     });
 
     return (
       <table>
         <caption>Amortization Schedule</caption>
         <thead>
-          {AmortizationScheduleItemComponent}
+          <tr>
+            <th>Installment</th>
+            <th>Installment Date</th>
+            <th>Payment</th>
+            <th>Interest Amount</th>
+            <th>Principal Amount</th>
+            <th>Balance</th>
+          </tr>
         </thead>
         <tbody>
+          {AmortizationScheduleItemComponent}
         </tbody>
-        <tfoot>
-        </tfoot>
       </table>
     );
   }
--- src/js-src/components/mortgage/amortizationScheduleItem.js
+++ src/js-src/components/mortgage/amortizationScheduleItem.js
@@ -5,20 +5,22 @@ export default class AmortizationScheduleItem extends Component {
 
   static propTypes = {
     installmentNumber: React.PropTypes.number.isRequired,
-    dueDate: React.PropTypes.string.isRequired,
+    installmentDate: React.PropTypes.string.isRequired,
+    payment: React.PropTypes.number.isRequired,
     interestAmount: React.PropTypes.number.isRequired,
     principalAmount: React.PropTypes.number.isRequired,
-    totalDue: React.PropTypes.number.isRequired
+    balance: React.PropTypes.number.isRequired
   }
 
   render() {
     return (
       <tr>
         <td>{this.props.installmentNumber}</td>
-        <td>{this.props.dueDate}</td>
+        <td>{this.props.installmentDate}</td>
+        <td>{this.props.payment.toFixed(2)}</td>
         <td>{this.props.interestAmount.toFixed(2)}</td>
         <td>{this.props.principalAmount.toFixed(2)}</td>
-        <td>{this.props.totalDue.toFixed(2)}</td>
+        <td>{this.props.balance.toFixed(2)}</td>
       </tr>
     );
   }
