448503f9e616583c582e3c83ab4451a3e41eadb7
Michael Astrauckas <mastrauckas@hotmail.com>
2017-03-08 20:39:50 -0500

    Making changes to the build process.

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,76 @@
 //To run in production, put in
 //NODE_ENV=production webpack
-const DEBUG = process.env.NODE_ENV !== 'production';
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const packages = require('./package.json');
+
+const PRODUCTION = process.env.NODE_ENV === 'production';
+const DEVELOPMENT = !PRODUCTION;
+
+const environment = process.env.NODE_ENV.toUpperCase();
 
 const colors = {
   reset: '\x1b[0m',
   fg: {
-
     red: '\x1b[31m'
   }
 }
 
-const outputFileName = DEBUG
+const outputFileName = DEVELOPMENT
   ? 'static/js/[name].bundle.js'
-  : 'static/js/[name].bundle.min.[chunkhash:8].js';
-const outputChunkFilename = DEBUG
+  : 'static/js/[name].bundle.[chunkhash:8].min.js';
+const outputChunkFilename = DEVELOPMENT
   ? './src/wwwroot/static/js/[name].bundle.chunk.js'
-  : './src/wwwroot/static/js/[name].bundle.min.chunk.[chunkhash:8].js';
+  : './src/wwwroot/static/js/[name].bundle.chunk.[chunkhash:8].min.js';
 
-if (DEBUG) {
-  console.log(colors.fg.red, 'Building in Development mode.', colors.reset);
-} else {
-  console.log(colors.fg.red, 'Building in Production mode.', colors.reset);
+const vendorPackages = Object.keys(packages.dependencies);
+
+const plugins = [
+  new HtmlWebpackPlugin({
+    template: './wwwroot/templates/index-before-scripts.html',
+    chunksSortMode: 'dependency',
+    filename: './templates/index.html'
+  })
+];
+
+if (PRODUCTION) {
+
+  plugins.push(new webpack.optimize.CommonsChunkPlugin({
+    names: ['vendor', 'manifest'],
+    minChunks: Infinity
+  }));
+
+  plugins.push(new webpack.optimize.OccurrenceOrderPlugin());
+
+  plugins.push(new webpack.optimize.UglifyJsPlugin({
+    sourceMap: false,
+    minimize: true,
+    beautify: false,
+    mangle: { screw_ie8: true, keep_fnames: true },
+    dead_code: true,
+    unused: true,
+    deadCode: true,
+    comments: false,
+    compress: {
+      screw_ie8: true,
+      keep_fnames: true,
+      drop_debugger: false,
+      dead_code: false,
+      unused: false,
+      warnings: false
+    }
+  }));
 }
+
+console.log(colors.fg.red, `Building in ${environment} mode.`, colors.reset);
 
 module.exports = {
   context: path.join(__dirname, '/src'),
-  devtool: DEBUG ? 'inline-source-map' : false,
+  devtool: DEVELOPMENT ? 'inline-source-map' : false,
   entry: {
     'app': './js-src/scripts.js',
-    'vendor': ['react', 'sugar', 'axios']
+    'vendor': vendorPackages
   },
   module: {
     rules: [
@@ -54,39 +93,5 @@ module.exports = {
     filename: outputFileName,
     chunkFilename: outputChunkFilename,
   },
-  plugins: DEBUG ? [
-    new HtmlWebpackPlugin({
-      template: 'index.html',
-      chunksSortMode: 'dependency'
-    }),
-  ] : [
-      new HtmlWebpackPlugin({
-        template: './wwwroot/templates/index-before-scripts.html',
-        chunksSortMode: 'dependency',
-        filename: './templates/index.html'
-      }),
-      new webpack.optimize.CommonsChunkPlugin({
-        names: ['vendor', 'manifest'],
-        minChunks: Infinity
-      }),
-      new webpack.optimize.OccurrenceOrderPlugin(),
-      new webpack.optimize.UglifyJsPlugin({
-        sourceMap: false,
-        minimize: true,
-        beautify: false,
-        mangle: { screw_ie8: true, keep_fnames: true },
-        dead_code: true,
-        unused: true,
-        deadCode: true,
-        comments: false,
-        compress: {
-          screw_ie8: true,
-          keep_fnames: true,
-          drop_debugger: false,
-          dead_code: false,
-          unused: false,
-          warnings: false
-        }
-      })
-    ],
+  plugins
 };
