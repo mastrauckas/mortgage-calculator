9f73ccd18f5e142d6d83ee7824a581395769c75c
Michael Astrauckas <mastrauckas@hotmail.com>
2017-03-16 23:16:20 -0400

    Moving label of 'Calculate My Mortgage' around.

--- src/js/components/mortgage/amortizationCalculator.js
+++ src/js/components/mortgage/amortizationCalculator.js
@@ -36,60 +36,68 @@ export default class AmortizationCalculator extends Component {
   render() {
     return (
       <div className='col l3'>
-        <div className='input-field'>
-          <label htmlFor='mortgagePrincipalAmount'>
-            Mortgage Principal Amount
-          </label>
-          <input id='mortgagePrincipalAmount'
-            type='text'
-            defaultValue={`$${this.schedule.principalAmount.format(2)}`}
-            ref={(input) => { this.domElements.principalAmount = input; }}>
-          </input>
+        <div>
+          <h4>Calulate My Mortgage</h4>
         </div>
-        <div className='input-field'>
-          <label htmlFor='mortgageTerm'>
-            Mortgage Term
+        <div id='calculator'>
+          <div className='input-field'>
+            <label htmlFor='mortgagePrincipalAmount'>
+              Mortgage Principal Amount
           </label>
-          <input id='mortgageTerm'
-            type='text'
-            defaultValue={this.schedule.installments}
-            ref={(input) => { this.domElements.installments = input; }}>
-          </input>
-        </div>
-        <div className='input-field'>
-          <label htmlFor='mortgagePayment'>
-            Mortgage Payment
+            <input id='mortgagePrincipalAmount'
+              type='text'
+              defaultValue={`$${this.schedule.principalAmount.format(2)}`}
+              ref={(input) => { this.domElements.principalAmount = input; }}>
+            </input>
+          </div>
+          <div className='input-field'>
+            <label htmlFor='mortgageTerm'>
+              Mortgage Term
+          </label>
+            <input id='mortgageTerm'
+              type='text'
+              defaultValue={this.schedule.installments}
+              ref={(input) => { this.domElements.installments = input; }}>
+            </input>
+          </div>
+          <div className='input-field'>
+            <label htmlFor='mortgagePayment'>
+              Mortgage Payment
         </label>
-          <input id='mortgagePayment'
-            type='text'
-            defaultValue={`$${this.schedule.payment.format(2)}`}
-            ref={(input) => { this.domElements.payment = input; }}>
-          </input>
-        </div>
-        <div className='input-field'>
-          <label htmlFor='mortgageStartDate'>
-            Mortgage Start Date
+            <input id='mortgagePayment'
+              type='text'
+              defaultValue={`$${this.schedule.payment.format(2)}`}
+              ref={(input) => { this.domElements.payment = input; }}>
+            </input>
+          </div>
+          <div className='input-field'>
+            <label htmlFor='mortgageStartDate'>
+              Mortgage Start Date
           </label>
-          <input id='mortgageStartDate'
-            type='text'
-            defaultValue={this.schedule.startDate}
-            ref={(input) => { this.domElements.startDate = input; }}>
-          </input>
-        </div>
-        <div className='input-field'>
-          <label htmlFor='mortgageInterestRate'>
-            Interest Rate
+            <input id='mortgageStartDate'
+              type='text'
+              defaultValue={this.schedule.startDate}
+              ref={(input) => { this.domElements.startDate = input; }}>
+            </input>
+          </div>
+          <div className='input-field'>
+            <label htmlFor='mortgageInterestRate'>
+              Interest Rate
           </label>
-          <input id='mortgageInterestRate'
-            type='text'
-            defaultValue={`${this.schedule.interestRate}%`}
-            ref={(input) => { this.domElements.interestRate = input; }}>
-          </input>
-        </div>
-        <button onClick={this.onClick.bind(this)}
-          className='waves-effect waves-light btn-large'>
-          Calculate My Mortgage
+            <input id='mortgageInterestRate'
+              type='text'
+              defaultValue={`${this.schedule.interestRate}%`}
+              ref={(input) => { this.domElements.interestRate = input; }}>
+            </input>
+          </div>
+          <div className='center'>
+            <button id='calculateMortgage'
+              onClick={this.onClick.bind(this)}
+              className='waves-effect waves-light btn'>
+              Calculate
         </button>
+          </div>
+        </div>
       </div>
     );
   }
