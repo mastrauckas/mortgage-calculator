ed2d46157928d2cab27fb5dc9a42aeb40a35d68d
Michael Astrauckas <mastrauckas@hotmail.com>
2017-03-17 19:35:06 -0400

    Breaking out test classes in their own files.

--- tests/py/test_mortgage_on_instance_method.py
+++ tests/py/test_mortgage_on_instance_method.py
@@ -1,48 +1,7 @@
 #!/usr/bin/env python3
+from dateutil import relativedelta
 from mortgage import Mortgage
 from datetime import datetime
-from dateutil import relativedelta
-
-
-class TestMortgageOnStaticMethods:
-    __rate = 3.75
-    __total_Installments = 360
-    __loan_Amount = 153705
-    __payment = 711.83
-
-    def test_get_Payment_Amount(self):
-        assert Mortgage.get_Payment_Amount(self.__rate,
-                                           self.__total_Installments,
-                                           self.__loan_Amount) \
-            == self.__payment
-
-    def test_monthly_Interest_Rate(self):
-        assert Mortgage.monthly_Interest_Rate(self.__rate) == 0.003125
-
-    def test_total_Installments(self):
-        assert Mortgage.total_Installments(30) == self.__total_Installments
-
-    def test_principal_On_Payment(self):
-        assert Mortgage.principal_On_Payment(self.__loan_Amount,
-                                             self.__payment,
-                                             self.__rate) == 231.50
-
-    def test_interest_On_Payment(self):
-        assert Mortgage.interest_On_Payment(self.__loan_Amount, self.__rate) \
-            == 480.33
-
-    def test_get_Installment(self):
-        payment_Date = datetime.strptime('1/29/2017', '%m/%d/%Y')
-        installment = Mortgage.get_Installment(1, payment_Date,
-                                               self.__loan_Amount,
-                                               self.__payment,
-                                               self.__rate)
-        assert installment.installment_Number() == 1
-        assert installment.payment_Date() == \
-            datetime.strptime('1/29/2017', '%m/%d/%Y')
-        assert installment.interest_Amount() == 480.33
-        assert installment.principal_Amount() == 231.50
-        assert installment.total_Payment() == self.__payment
 
 
 class TestMortgageOnInstanceMethods:
