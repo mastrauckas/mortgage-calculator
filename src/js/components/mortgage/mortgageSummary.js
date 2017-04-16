d396d63de7238734271ff019d511b8b1bc81dd47
Michael Astrauckas <mastrauckas@hotmail.com>
2017-04-16 09:20:34 -0400

    Putting different sections on the summary page for a better breakdown.

--- src/js/components/mortgage/mortgageSummary.js
+++ src/js/components/mortgage/mortgageSummary.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Component } from 'react';
-import { Card, CardTitle } from 'material-ui/Card';
-import { Table, TableBody, TableRow, TableRowColumn } from 'material-ui/Table';
+import { Card } from 'material-ui/Card';
+import Paper from 'material-ui/Paper';
+import { Table, TableBody, TableRow, TableRowColumn, TableFooter } from 'material-ui/Table';
+import { Grid, Row, Col } from 'react-flexbox-grid';
 import Sugar from 'sugar';
 
 export default class MortgageSummary extends Component {
@@ -28,85 +30,129 @@ export default class MortgageSummary extends Component {
 
   render() {
     return (
-      <Card>
-        <CardTitle title="Your Mortgage Summary Information" />
-        <Table selectable={false}>
-          <TableBody displayRowCheckbox={false}>
-            <TableRow>
-              <TableRowColumn style={{ textAlign: 'left' }}>
-                <h2>Mortgage Principal</h2>
-              </TableRowColumn>
-              <TableRowColumn style={{ textAlign: 'right' }}>
-                <h2>${(this.props.principalAmount || 0).format(2)}</h2>
-              </TableRowColumn>
-            </TableRow>
-            <TableRow>
-              <TableRowColumn style={{ textAlign: 'left' }}>
-                <h2>Total Interest</h2>
-              </TableRowColumn>
-              <TableRowColumn style={{ textAlign: 'right' }}>
-                <h2>${(this.props.totalInterest || 0).format(2)}</h2>
-              </TableRowColumn>
-            </TableRow>
-            <TableRow>
-              <TableRowColumn style={{ textAlign: 'left' }}>
-                <h2>Total Paid</h2>
-              </TableRowColumn>
-              <TableRowColumn style={{ textAlign: 'right' }}>
-                <h2>${(this.props.totalPaid || 0).format(2)}</h2>
-              </TableRowColumn>
-            </TableRow>
-            <TableRow>
-              <TableRowColumn style={{ textAlign: 'left' }}>
-                <h2>Percent Paid In Interest</h2>
-              </TableRowColumn>
-              <TableRowColumn style={{ textAlign: 'right' }}>
-                <h2>{(this.props.percentInterest || 0).format(2)}%</h2>
-              </TableRowColumn>
-            </TableRow>
-            <TableRow>
-              <TableRowColumn style={{ textAlign: 'left' }}>
-                <h2>Principal Turnover Date</h2>
-              </TableRowColumn>
-              <TableRowColumn style={{ textAlign: 'right' }}>
-                <h2>{(this.props.turnOverDate || 'n/a').format('%m/%d/%Y')}</h2>
-              </TableRowColumn>
-            </TableRow>
-            <TableRow>
-              <TableRowColumn style={{ textAlign: 'left' }}>
-                <h2>Principal Turnover Timeframe</h2>
-              </TableRowColumn>
-              <TableRowColumn style={{ textAlign: 'right' }}>
-                <h2>{this.props.turnOverYearsAndMonths || ''}</h2>
-              </TableRowColumn>
-            </TableRow>
-            <TableRow>
-              <TableRowColumn style={{ textAlign: 'left' }}>
-                <h2>Total Mortgage Payments</h2>
-              </TableRowColumn>
-              <TableRowColumn style={{ textAlign: 'right' }}>
-                <h2>{this.props.mortgageTotalPayments || ''}</h2>
-              </TableRowColumn>
-            </TableRow>
-            <TableRow>
-              <TableRowColumn style={{ textAlign: 'left' }}>
-                <h2>Total Mortgage Time Length</h2>
-              </TableRowColumn>
-              <TableRowColumn style={{ textAlign: 'right' }}>
-                <h2>{this.props.mortgageTotalYearsAndMonth || ''}</h2>
-              </TableRowColumn>
-            </TableRow>
-            <TableRow>
-              <TableRowColumn style={{ textAlign: 'left' }}>
-                <h2>Last Payment Date</h2>
-              </TableRowColumn>
-              <TableRowColumn style={{ textAlign: 'right' }}>
-                <h2>{(this.props.lastPaymentDate || 'n/a').format('%m/%d/%Y')}</h2>
-              </TableRowColumn>
-            </TableRow>
-          </TableBody>
-        </Table>
-      </Card>
+      <Paper zDepth={3}>
+        <Grid>
+          <Row center='md'>
+            <Col>
+              <h2>Mortgage Investment Information</h2>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Table selectable={false}>
+                <TableBody displayRowCheckbox={false}>
+                  <TableRow>
+                    <TableRowColumn style={{ textAlign: 'left' }}>
+                      <div style={{ fontSize: '1.5em' }}>Mortgage Principal</div>
+                    </TableRowColumn>
+                    <TableRowColumn style={{ textAlign: 'right' }}>
+                      <div style={{ fontSize: '1.5em' }}>${(this.props.principalAmount || 0).format(2)}</div>
+                    </TableRowColumn>
+                  </TableRow>
+                  <TableRow>
+                    <TableRowColumn style={{ textAlign: 'left' }}>
+                      <div style={{ fontSize: '1.5em' }}>Total Interest</div>
+                    </TableRowColumn>
+                    <TableRowColumn style={{ textAlign: 'right' }}>
+                      <div style={{ fontSize: '1.5em' }}>${(this.props.totalInterest || 0).format(2)}</div>
+                    </TableRowColumn>
+                  </TableRow>
+                  <TableRow>
+                    <TableRowColumn style={{ textAlign: 'left' }}>
+                      <div style={{ fontSize: '1.5em' }}>Percent Paid In Interest</div>
+                    </TableRowColumn>
+                    <TableRowColumn style={{ textAlign: 'right' }}>
+                      <div style={{ fontSize: '1.5em' }}>{(this.props.percentInterest || 0).format(2)}%</div>
+                    </TableRowColumn>
+                  </TableRow>
+                </TableBody>
+                <TableFooter adjustForCheckbox={false}>
+                  <TableRow>
+                    <TableRowColumn>
+                      <h2>Total Paid</h2>
+                    </TableRowColumn>
+                    <TableRowColumn style={{ textAlign: 'right' }}>
+                      <h2>${(this.props.totalPaid || 0).format(2)}</h2>
+                    </TableRowColumn>
+                  </TableRow>
+                </TableFooter>
+              </Table>
+            </Col>
+          </Row>
+        </Grid>
+
+        <Grid>
+          <Row center='md'>
+            <Col>
+              <h2>Mortgage End Information</h2>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Table selectable={false}>
+                <TableBody displayRowCheckbox={false}>
+                  <TableRow>
+                    <TableRowColumn style={{ textAlign: 'left' }}>
+                      <div style={{ fontSize: '1.5em' }}>Total Mortgage Time Length</div>
+                    </TableRowColumn>
+                    <TableRowColumn style={{ textAlign: 'right' }}>
+                      <div style={{ fontSize: '1.5em' }}>{this.props.mortgageTotalYearsAndMonth || ''}</div>
+                    </TableRowColumn>
+                  </TableRow>
+                  <TableRow>
+                    <TableRowColumn style={{ textAlign: 'left' }}>
+                      <div style={{ fontSize: '1.5em' }}>Last Payment Date</div>
+                    </TableRowColumn>
+                    <TableRowColumn style={{ textAlign: 'right' }}>
+                      <div style={{ fontSize: '1.5em' }}>{(this.props.lastPaymentDate || 'n/a').format('%m/%d/%Y')}</div>
+                    </TableRowColumn>
+                  </TableRow>
+                  <TableRow>
+                    <TableRowColumn style={{ textAlign: 'left' }}>
+                      <div style={{ fontSize: '1.5em' }}>Total Mortgage Payments</div>
+                    </TableRowColumn>
+                    <TableRowColumn style={{ textAlign: 'right' }}>
+                      <div style={{ fontSize: '1.5em' }}>{this.props.mortgageTotalPayments || ''}</div>
+                    </TableRowColumn>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </Col>
+          </Row>
+        </Grid>
+
+        <Grid>
+          <Row center='md'>
+            <Col>
+              <h2>More Principal Than Interest</h2>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Table selectable={false}>
+                <TableBody displayRowCheckbox={false}>
+                  <TableRow>
+                    <TableRowColumn style={{ textAlign: 'left' }}>
+                      <div style={{ fontSize: '1.5em' }}>Principal Turnover Date</div>
+                    </TableRowColumn>
+                    <TableRowColumn style={{ textAlign: 'right' }}>
+                      <div style={{ fontSize: '1.5em' }}>{(this.props.turnOverDate || 'n/a').format('%m/%d/%Y')}</div>
+                    </TableRowColumn>
+                  </TableRow>
+                  <TableRow>
+                    <TableRowColumn style={{ textAlign: 'left' }}>
+                      <div style={{ fontSize: '1.5em' }}>Principal Turnover Timeframe</div>
+                    </TableRowColumn>
+                    <TableRowColumn style={{ textAlign: 'right' }}>
+                      <div style={{ fontSize: '1.5em' }}>{this.props.turnOverYearsAndMonths || ''}</div>
+                    </TableRowColumn>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </Col>
+          </Row>
+        </Grid>
+      </Paper>
     );
   }
 }
