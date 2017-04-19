eb793f1d407566bbc7dba49517294c5bd25f408f
Michael Astrauckas <mastrauckas@hotmail.com>
2017-04-18 23:13:46 -0400

    Making the footer look better and adding a link to github repo.

--- src/js/components/layout/footer.js
+++ src/js/components/layout/footer.js
@@ -5,15 +5,22 @@ import { Component } from 'react';
 export default class Footer extends Component {
   render() {
     const style = {
-      backgroundColor: '#0A2A22',
+      backgroundColor: '#38464D',
+      verticalAlign: 'center',
+      marginTop: '25px',
       height: '80px',
+      display: 'flex',
+      alignItems: 'center'
     };
 
     return (
       <footer style={style} target='_black' title='Github Repoistory'>
-        <h5 style={{ color: '#FAFAFA', paddingTop: '24px', paddingLeft: '24px' }}>
+        <h5 style={{ color: 'rgba(255, 255, 255, 0.8)', paddingLeft: '24px' }}>
           <span>Developed by Michael Astrauckas under MIT License</span>
-          <a className='.github_icon' href='https://github.com/mastrauckas/mortgage-playground'></a>
+          <a className='github_icon'
+            target='_blank'
+            title='Github Repoistory'
+            href='https://github.com/mastrauckas/mortgage-playground'></a>
         </h5>
       </footer >
     );
