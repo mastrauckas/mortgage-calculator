e7ca01c8cb13e0463227bfe653672a2849596a31
Michael Astrauckas <mastrauckas@hotmail.com>
2017-12-16 14:16:59 -0500

    Make it where I can build docker images.

---
+++ docker/build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+rm -r ./api/build
+cd ../api/src/Maa.MortgageCalculator.Endpoints
+dotnet publish -c Release -o ../../../docker/api/build
+
+cd ../../../web
+yarn build:docker
+
+cd ../docker
+docker-compose build
